302c1835e36df21fbfab0d646f67ae24cb0a7a5d
madjarevicn <nikola.madjarevic@gmail.com>
2021-07-11 13:52:20 +0300

    Remove floating pragma.

--- contracts/upgradabilityTest/HordTicketFactoryV2.sol
+++ contracts/upgradabilityTest/HordTicketFactoryV2.sol
@@ -1,5 +1,5 @@
 //"SPDX-License-Identifier: UNLICENSED"
-pragma solidity ^0.6.12;
+pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155PausableUpgradeable.sol";
 import "../interfaces/IHordTicketManager.sol";
