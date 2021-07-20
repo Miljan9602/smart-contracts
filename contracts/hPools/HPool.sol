d813e0730275fd8f23280996bbe95313519ec933
madjarevicn <nikola.madjarevic@gmail.com>
2021-07-20 10:33:40 +0200

    Add HPool deposit functions.

--- contracts/hPools/HPool.sol
+++ contracts/hPools/HPool.sol
@@ -1,14 +1,49 @@
 pragma solidity 0.6.12;
 
+import "../system/HordMiddleware.sol";
+
 /**
  * HPool contract.
  * @author Nikola Madjarevic
  * Date created: 20.7.21.
  * Github: madjarevicn
  */
-contract HPool {
+contract HPool is HordMiddleware {
 
-    constructor () public {
+    address public hPoolManager;
 
+    event FollowersBudgetDeposit(uint256 amount);
+    event ChampionBudgetDeposit(uint256 amount);
+
+    modifier onlyHPoolManager {
+        require(msg.sender == hPoolManager);
+        _;
+    }
+
+    constructor(
+        address _hordCongress,
+        address _hordMaintainersRegistry
+    )
+    public
+    {
+        setCongressAndMaintainers(_hordCongress, _hordMaintainersRegistry);
+        hPoolManager = msg.sender;
+    }
+
+
+    function depositBudgetFollowers()
+    external
+    onlyHPoolManager
+    payable
+    {
+        emit FollowersBudgetDeposit(msg.value);
+    }
+
+    function depositBudgetChampion()
+    external
+    onlyHPoolManager
+    payable
+    {
+        emit ChampionBudgetDeposit(msg.value);
     }
 }
