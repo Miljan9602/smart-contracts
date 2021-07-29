eee981a934f80c6f77e674cc8e852a5fa568ff9f
madjarevicn <nikola.madjarevic@gmail.com>
2021-07-29 10:51:08 +0200

    Add HPoolFactory.sol contract and it's interface.

---
+++ contracts/interfaces/IHPoolFactory.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.6.12;
+
+/**
+ * IHPoolFactory contract.
+ * @author Nikola Madjarevic
+ * Date created: 29.7.21.
+ * Github: madjarevicn
+ */
+interface IHPoolFactory {
+    function deployHPool() external returns (address);
+}
