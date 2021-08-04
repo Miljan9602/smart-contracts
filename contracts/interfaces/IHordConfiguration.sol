11e0bdd71fae56f8cea9825f1481977fd23ef32b
markolazic01 <marko@2key.co>
2021-08-04 20:01:47 +0200

    IHordConfiguration.sol add getters.

--- contracts/interfaces/IHordConfiguration.sol
+++ contracts/interfaces/IHordConfiguration.sol
@@ -7,5 +7,14 @@ pragma solidity 0.6.12;
  * Github: madjarevicn
  */
 interface IHordConfiguration {
-
-}
+    function minChampStake() external view returns(uint256);
+    function maxWarmupPeriod() external view returns(uint256);
+    function maxFollowerOnboardPeriod() external view returns(uint256);
+    function minFollowerEthStake() external view returns(uint256);
+    function maxFollowerEthStake() external view returns(uint256);
+    function minStakePerPoolTicket() external view returns(uint256);
+    function assetUtilizationRatio() external view returns(uint256);
+    function gasUtilizationRatio() external view returns(uint256);
+    function platformStakeRatio() external view returns(uint256);
+    function maxSupplyHPoolToken() external view returns(uint256);
+    }
