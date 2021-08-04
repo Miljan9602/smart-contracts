087cdd1eff76cb2903ef7c59844718d9df4f768f
madjarevicn <nikola.madjarevic@gmail.com>
2021-08-04 19:55:42 +0200

    Add IHordConfiguration.sol interface.

---
+++ contracts/interfaces/IHordConfiguration.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.6.12;
+
+/**
+ * IHordConfiguration contract.
+ * @author Nikola Madjarevic
+ * Date created: 4.8.21.
+ * Github: madjarevicn
+ */
+interface IHordConfiguration {
+
+}
