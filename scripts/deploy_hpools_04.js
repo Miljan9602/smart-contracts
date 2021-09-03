f3b4182deb4b86bef78a5262140352d7ef85b8b4
s2imonovic <s2imonovic@gmail.com>
2021-09-03 15:56:51 +0200

    Fix name in deploy_hpools_04.js.

--- scripts/deploy_hpools_04.js
+++ scripts/deploy_hpools_04.js
@@ -55,7 +55,7 @@ async function main() {
     ]);
     await hPoolManager.deployed();
     console.log('HPoolManager is deployed to:', hPoolManager.address);
-    saveContractProxies(hre.network.name, 'HPoolManager', hPoolFactory.address);
+    saveContractProxies(hre.network.name, 'HPoolManager', hPoolManager.address);
 
     // Setters
     await hPoolFactory.setHPoolManager(hPoolManager.address);
