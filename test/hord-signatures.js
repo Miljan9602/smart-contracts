c8f78874a6a585d6c3c6a2376f090b435025aa5f
s2imonovic <s2imonovic@gmail.com>
2021-10-11 22:55:36 +0200

    Change tests for signatures.

--- test/hord-signatures.js
+++ test/hord-signatures.js
@@ -3,16 +3,15 @@ const {
 } = require('./ethereum');
 const hre = require("hardhat");
 let configuration = require('../deployments/deploymentConfig.json');
-const { ethers, expect } = require('./setup');
-const util = require('ethereumjs-util')
-
+const { ethers, expect, BigNumber } = require('./setup');
+const util = require('ethereumjs-util');
 
 let DOMAIN_SEPARATOR;
 let config;
 let accounts, owner, ownerAddr, tokenA, tokenAAddrs, tokenB, tokenBAddr;
 let signatureValidator;
+let recoveredAddress, signature;
 let r, s, v;
-let recoveredAddress;
 
 async function setupContractAndAccounts () {
     config = configuration[hre.network.name];
@@ -31,7 +30,7 @@ async function setupContractAndAccounts () {
 
 }
 
-async function getSignature(message, type, primaryType) {
+async function getSignature(message, primaryType, type) {
 
     // const data = {
     //     domain: {
@@ -64,134 +63,143 @@ async function getSignature(message, type, primaryType) {
     //     from,
     // });
     //
-    // const signature = result.substring(2);
-    // const r = `0x${signature.substring(0, 64)}`;
-    // const s = `0x${signature.substring(64, 128)}`;
-    // let v = parseInt(signature.substring(128, 130), 16);
-    // v = v < 27 ? v + 27 : v;
-
-    const hexPrivateKey = "0xae78c8b502571dba876742437f8bc78b689cf8518356c0921393d89caaf284ce";
-    const signingKey = new ethers.utils.SigningKey(hexPrivateKey);
-    const digest = ethers.utils.id("Some message");
-    const signature = signingKey.signDigest(digest);
-    const joinedSignature = ethers.utils.joinSignature(signature);
-    recoveredAddress = ethers.utils.recoverAddress(digest, signature);
-
-    //const pubKey = util.ecrecover(new Buffer('aaa'), signature.v, signature.r, signature.s);
-
-
-    console.log(pubKey);
-
-    r = signature.r;
-    s = signature.s;
-    v = signature.v;
-
-    console.log(r);
-    console.log(s);
-    console.log(v);
-
-    // const result = "0xa9679464a5f5ee62b51def858199a040af52804eef9f1e05a0645df77953dbb611d6cf7da810fa6626d0aebc785768bb91f746b49b856d95feb3e7c94bfcc86b21"
-    // const signature = result.substring(2);
+    // signature = result.substring(2);
     // r = `0x${signature.substring(0, 64)}`;
     // s = `0x${signature.substring(64, 128)}`;
     // v = parseInt(signature.substring(128, 130), 16);
     // v = v < 27 ? v + 27 : v;
+
+    // const hexPrivateKey = "0xae78c8b502571dba876742437f8bc78b689cf8518356c0921393d89caaf284ce";
+    // const signingKey = new ethers.utils.SigningKey(hexPrivateKey);
+    // const digest = ethers.utils.id("message");
+    // signature = signingKey.signDigest(digest);
+    // const joinedSignature = ethers.utils.joinSignature(signature);
+    // recoveredAddress = ethers.utils.recoverAddress(digest, signature);
+    //
+    // r = signature.r;
+    // s = signature.s;
+    // v = signature.v;
     //
     // console.log(r);
     // console.log(s);
     // console.log(v);
 
-    // var Eth = require('web3-eth');
-    // var eth = new Eth(Eth.givenProvider || 'ws://some.local-or-remote.node:8546');
-    //
-    // var message = 1001
-    // var orgmessage = web3.eth.abi.encodeParameter('uint256',message)
-    // var orginal = `0xf8e81D47203A594245E36C48e151709F0C19fBe8${orgmessage.slice(2)}`
-    // var hash = web3.utils.keccak256(orginal);
-    // const accounts = await ethers.getSigners()[0];
-    // var signature = await web3.eth.personal.sign(hash, accounts);
-    // var sig1 = signature.slice(2)
-    // var r = `0x${sig1.slice(0, 64)}`
-    // var s = `0x${sig1.slice(64, 128)}`
-    // var v = web3.utils.toDecimal(`0x${sig1.slice(128, 130)}`)
-    //
-    // console.log("h",hash)
-    // console.log("v",v)
-    // console.log("r",r)
-    // console.log("s",s)
+
+    let Example = require('/home/srdjan/Desktop/hord/smart-contracts/test/hord-signatures.js')
+
+    let Web3 = require('web3')
+    let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+    contract('Example', (accounts) => {
+        let address = accounts[0]
+
+        it('ecrecover result matches address', async function() {
+            let instance = await Example.deployed()
+            let msg = '0x8CbaC5e4d803bE2A3A5cd3DbE7174504c6DD0c1C'
+
+            let h = web3.sha3(msg)
+            let sig = web3.eth.sign(address, h).slice(2)
+            let r = `0x${sig.slice(0, 64)}`
+            let s = `0x${sig.slice(64, 128)}`
+            let v = web3.toDecimal(sig.slice(128, 130)) + 27
+
+            let result = await instance.testRecovery.call(h, v, r, s)
+            assert.equal(result, address)
+        })
+    })
+
+    // const pubKey = util.ecrecover(signature.v, signature.r, signature.s);
+    // console.log(pubKey);
+}
+
+async function invalidSignature() {
+    let result = "0x196bc98ba48f6880183a85e2fccfb8475ba27451804be8d7327426c92b1b47c4bf7ec98258a405fcdb6e87da36f3d26f4de98e299f6ce8e02ab403ffc787d481b4"
+
+    signature = result.substring(2);
+    r = `0x${signature.substring(0, 64)}`;
+    s = `0x${signature.substring(64, 128)}`;
+    v = parseInt(signature.substring(128, 130), 16);
+    v = v < 27 ? v + 27 : v;
 }
 
 describe('HordSignatures', async() => {
 
     before('setup contracts', async () => {
         await setupContractAndAccounts();
-        await getSignature();
         DOMAIN_SEPARATOR = await signatureValidator.DOMAIN_SEPARATOR();
     });
 
-    describe('HordSignatures:: BuyOrderRatio', async() => {
+    describe('Valid signature', async() => {
 
-        it('should check if return values is 0x0 address in recoverSignatureBuyOrderRatio function', async() => {
-            let returnValue = await signatureValidator.recoverSignatureBuyOrderRatio([tokenAAddrs, 2], r, s, v);
-            console.log(returnValue);
+        before('Sign transaction', async() => {
+            await getSignature();
         });
 
-        it('should check if return values is not correct in recoverSignatureBuyOrderRatio function', async() => {
-
+        describe('HordSignatures:: BuyOrderRatio', async() => {
+            it('should check if return value is correct value in recoverSignatureBuyOrderRatio function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureBuyOrderRatio([tokenAAddrs, 2], r, s, v);
+                console.log(recoveredAddress);
+            });
         });
 
-        it('should check if return value is correct value in recoverSignatureBuyOrderRatio function', async() => {
+        describe('HordSignatures:: TradeOrder', async() => {
+            it('should check if return value is correct value in recoverSignatureTradeOrder function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureTradeOrder([tokenAAddrs, tokenBAddr, 10], r, s, v);
+                console.log(recoveredAddress);
+            });
+        });
 
+        describe('HordSignatures:: SellLimit', async() => {
+            it('should check if return value is correct value in recoverSignatureSellLimit function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureSellLimit([tokenAAddrs, tokenBAddr, 100, 10, 10000], r, s, v);
+                console.log(recoveredAddress);
+            });
+        });
+
+        describe('HordSignatures:: BuyLimit', async() => {
+            it('should check if return value is correct in recoverSignatureBuyLimit function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureBuyLimit([tokenAAddrs, tokenBAddr, 100, 10, 10000], r, s, v);
+                console.log(recoveredAddress);
+            });
         });
     });
 
-    // describe('HordSignatures:: TradeOrder', async() => {
-    //
-    //     it('should check if return values is 0x0 address in recoverSignatureTradeOrder function', async() => {
-    //         let returnValue = await signatureValidator.recoverSignatureTradeOrder([tokenAAddrs, tokenBAddr, 10], r, s, v);
-    //         console.log(returnValue);
-    //     });
-    //
-    //     it('should check if return values is not correct in recoverSignatureTradeOrder function', async() => {
-    //
-    //     });
-    //
-    //     it('should check if return value is correct value in recoverSignatureTradeOrder function', async() => {
-    //
-    //     });
-    // });
-    //
-    //
-    // describe('HordSignatures:: SellLimit', async() => {
-    //
-    //     it('should check if return values is 0x0 address in recoverSignatureSellLimit function', async() => {
-    //         let returnValue = await signatureValidator.recoverSignatureSellLimit([tokenAAddrs, tokenBAddr, 100, 10, 10000], r, s, v);
-    //         console.log(returnValue);
-    //     });
-    //
-    //     it('should check if return values is not correct in recoverSignatureSellLimit function', async() => {
-    //
-    //     });
-    //
-    //     it('should check if return value is correct value in recoverSignatureSellLimit function', async() => {
-    //
-    //     });
-    // });
-    //
-    // describe('HordSignatures:: BuyLimit', async() => {
-    //
-    //     it('should check if return values is 0x0 address in recoverSignatureBuyLimit function', async() => {
-    //         let returnValue = await signatureValidator.recoverSignatureBuyLimit([tokenAAddrs, tokenBAddr, 100, 10, 10000], r, s, v);
-    //         console.log(returnValue);
-    //     });
-    //
-    //     it('should check if return values is not correct in recoverSignatureBuyLimit function', async() => {
-    //
-    //     });
-    //
-    //     it('should check if return value is correct in recoverSignatureBuyLimit function', async() => {
-    //
-    //     });
-    // });
+    describe('Invalid signatures', async() => {
+        before('Sign transaction', async() => {
+            await invalidSignature();
+        });
+
+        describe('HordSignatures:: BuyOrderRatio', async() => {
+            it('should check if return values is 0x0 address in recoverSignatureBuyOrderRatio function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureBuyOrderRatio([tokenAAddrs, 2], r, s, v);
+                expect(recoveredAddress)
+                    .to.be.equal(address(0));
+            });
+        });
+
+        describe('HordSignatures:: TradeOrder', async() => {
+            it('should check if return values is 0x0 address in recoverSignatureTradeOrder function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureTradeOrder([tokenAAddrs, tokenBAddr, 10], r, s, v);
+                expect(recoveredAddress)
+                    .to.be.equal(address(0));
+            });
+        });
+
+        describe('HordSignatures:: SellLimit', async() => {
+            it('should check if return values is 0x0 address in recoverSignatureSellLimit function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureSellLimit([tokenAAddrs, tokenBAddr, 100, 10, 10000], r, s, v);
+                expect(recoveredAddress)
+                    .to.be.equal(address(0));
+            });
+        });
+
+        describe('HordSignatures:: BuyLimit', async() => {
+            it('should check if return values is 0x0 address in recoverSignatureBuyLimit function', async() => {
+                recoveredAddress = await signatureValidator.recoverSignatureBuyLimit([tokenAAddrs, tokenBAddr, 100, 10, 10000], r, s, v);
+                expect(recoveredAddress)
+                    .to.be.equal(address(0));
+            });
+        });
+    });
 
 });
